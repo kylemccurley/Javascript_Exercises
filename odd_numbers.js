afea81ffe45092364b352f3f0fc07a4c0965a826
Kyle McCurley <kmccurley123@gmail.com>
2020-07-22 02:37:36 -0400

    Create odd_number.js

---
+++ odd_numbers.js
@@ -0,0 +1,21 @@
+function logOddNumbers(max) {
+  for(i = 1; i <= max; i += 2) {
+    console.log(i);
+  }
+}
+
+logOddNumbers(19);
+
+// output on console
+/*
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+*/
