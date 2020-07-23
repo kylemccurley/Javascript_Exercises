6f69147824b5d264cb739e0b76b23f9fc1163805
Kyle McCurley <kmccurley123@gmail.com>
2020-07-23 01:23:07 -0400

    Update trim_spaces.js

--- trim_spaces.js
+++ trim_spaces.js
@@ -1,18 +1,42 @@
 function trim(string) {
   let copy = false;
-  let outcome = '';
+  let leftTrim = '';
 
+  // Trim Leftmost Side
   for(i = 0; i < string.length; i++) {
     let char = string[i];
 
     if (!copy && char == ' ') {
       continue;
     } else if (!copy && char != ' ') {
-      outcome += char;
+      leftTrim += char;
       copy = true;
     } else {
-      outcome += char;
+      leftTrim += char;
     }
+  }
+
+  // Trim RightMost Side (Final Outcome)
+  copy = false;
+  let rightTrim = '';
+
+  for(i = (leftTrim.length - 1); i >= 0; i--) {
+    let char = leftTrim[i];
+
+    if (!copy && char == ' ') {
+      continue;
+    } else if (!copy && char != ' ') {
+      rightTrim += char;
+      copy = true;
+    } else {
+      rightTrim += char;
+    }
+  }
+
+  // Reverse the outcome
+  let outcome = '';
+  for (i = (rightTrim.length - 1); i >= 0; i--) {
+    outcome += rightTrim[i];
   }
 
   return outcome;
