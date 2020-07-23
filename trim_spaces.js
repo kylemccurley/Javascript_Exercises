85dee753bc8d5f420e337a04b33bbba8f5caae6d
Kyle McCurley <kmccurley123@gmail.com>
2020-07-23 01:02:33 -0400

    Update trim_spaces.js

--- trim_spaces.js
+++ trim_spaces.js
@@ -1,1 +1,26 @@
+function trim(string) {
+  let copy = false;
+  let outcome = '';
 
+  for(i = 0; i < string.length; i++) {
+    let char = string[i];
+
+    if (!copy && char == ' ') {
+      continue;
+    } else if (!copy && char != ' ') {
+      outcome += char;
+      copy = true;
+    } else {
+      outcome += char;
+    }
+  }
+
+  return outcome;
+}
+
+console.log(trim('  abc  '));  // "abc"
+console.log(trim('abc   '));   // "abc"
+console.log(trim(' ab c'));    // "ab c"
+console.log(trim(' a b  c'));  // "a b  c"
+console.log(trim('      '));   // ""
+console.log(trim(''));         // ""
