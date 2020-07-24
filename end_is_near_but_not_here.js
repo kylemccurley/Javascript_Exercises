3cd3555c1f4eb77f1554fd058297566a96441b47
Kyle McCurley <kmccurley123@gmail.com>
2020-07-24 01:28:43 -0400

    Create end_is_near_but_not_here.js

---
+++ end_is_near_but_not_here.js
@@ -0,0 +1,9 @@
+function penultimate(str) {
+  let words = str.split(' ');
+  let index = words.length - 2;
+
+  return words[index];
+}
+
+console.log(penultimate("last word") === "last"); // logs true
+console.log(penultimate("Launch School is great!") === "is"); // logs true
