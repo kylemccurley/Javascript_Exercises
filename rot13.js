63e66d47b909c5d729cbe77c55ab48b38f3c28de
Kyle McCurley <kmccurley123@gmail.com>
2020-07-23 22:08:50 -0400

    Create rot13.js

---
+++ rot13.js
@@ -0,0 +1,61 @@
+function rot13(string) {
+  const ROTATION_VALUE = 13;
+
+  // Check if the character ascii argument is in the last 13 characters of the alphabet (n-z).
+  let alphabetEnding = (charAscii) => {
+    // Bottom-most and upper-most ascii 
+    // values for alphabetical letters.
+    const UPPER_CASE_LOWER_BOUND_ASCII = 78;
+    const UPPER_CASE_UPPER_BOUND_ASCII = 90;
+  
+    const LOWER_CASE_LOWER_BOUND_ASCII = 110;
+    const LOWER_CASE_UPPER_BOUND_ASCII = 122;
+  
+    return ((charAscii >= UPPER_CASE_LOWER_BOUND_ASCII &&
+            charAscii <= UPPER_CASE_UPPER_BOUND_ASCII) ||
+           ( // LowerCase
+            charAscii >= LOWER_CASE_LOWER_BOUND_ASCII &&
+            charAscii <= LOWER_CASE_UPPER_BOUND_ASCII));
+  }
+
+  // Check if character argument is in the first 13 letters of the alphabet (a-m).
+  let alphabetBeginning = (charAscii) => {
+    // Bottom-most and upper-most ascii
+    // values for alphabetical letters.
+    const UPPER_CASE_LOWER_BOUND_ASCII = 65;
+    const UPPER_CASE_UPPER_BOUND_ASCII = 77;
+
+    const LOWER_CASE_LOWER_BOUND_ASCII = 97;
+    const LOWER_CASE_UPPER_BOUND_ASCII = 109;
+  
+    return ((charAscii >= UPPER_CASE_LOWER_BOUND_ASCII &&
+             charAscii <= UPPER_CASE_UPPER_BOUND_ASCII) ||
+             ( // Lowercase
+              charAscii >= LOWER_CASE_LOWER_BOUND_ASCII &&
+              charAscii <= LOWER_CASE_UPPER_BOUND_ASCII));
+  }
+  
+  // Perform translation based on character's
+  // position in the alphabet.
+  // Add rotation value or subtract?
+  let translateFrom = (ascii) => {
+    if (alphabetBeginning(ascii)) {
+      let translatedAscii = (ascii + ROTATION_VALUE);
+      return String.fromCharCode(translatedAscii);
+    } else if (alphabetEnding(ascii)) {
+      let translatedAscii = (ascii - ROTATION_VALUE);
+      return String.fromCharCode(translatedAscii);
+    } else {
+      return String.fromCharCode(ascii);
+    }
+  }
+
+// Primary Algorithm
+  let outcome = '';
+  for (let i = 0; i < string.length; i++) {
+    let ascii = string.charCodeAt(i);
+    outcome += translateFrom(ascii)
+  }
+
+  return outcome;
+}
