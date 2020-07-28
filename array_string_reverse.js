10ff63687696eb8f32632b5e0f8ea8311d2cbb0e
Kyle McCurley <kmccurley123@gmail.com>
2020-07-28 13:14:08 -0400

    Create array_string_reverse.js

---
+++ array_string_reverse.js
@@ -0,0 +1,28 @@
+function reverse(inputForReversal) {
+  if (typeof inputForReversal == 'string') {
+    var output = '';
+  } else {
+    var output = [];
+  }
+
+  for (let i = (inputForReversal.length - 1); i > -1; i--) {
+    let char = inputForReversal[i];
+    if (typeof inputForReversal == 'string') {
+      output += char;
+    } else {
+      output.push(char);
+    }
+  }
+
+  console.log(output);
+  return output;
+}
+
+reverse('Hello');           // "olleH"
+reverse('a');               // "a"
+reverse([1, 2, 3, 4]);      // [4, 3, 2, 1]
+reverse([]);                // []
+
+const array = [1, 2, 3];
+reverse(array);             // [3, 2, 1]
+array;                      // [1, 2, 3]
