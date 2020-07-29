9a962bc1cddbfb70a15b5178f7a69e164c024fda
Kyle McCurley <kmccurley123@gmail.com>
2020-07-29 00:15:59 -0400

    Create are_arrays_equal.js

---
+++ are_arrays_equal.js
@@ -0,0 +1,26 @@
+function areArraysEqual(array1, array2) {
+  let arr1 = Array.from(array1).sort();
+  let arr2 = Array.from(array2).sort();
+
+  for(let i = 0; i < array1.length; i++) {
+    let el1 = arr1[i];
+    let el2 = arr2[i];
+
+    if (!(el1 === el2)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+areArraysEqual([1, 2, 3], [1, 2, 3]);                  // true
+areArraysEqual([1, 2, 3], [3, 2, 1]);                  // true
+areArraysEqual(['a', 'b', 'c'], ['b', 'c', 'a']);      // true
+areArraysEqual(['1', 2, 3], [1, 2, 3]);                // false
+areArraysEqual([1, 1, 2, 3], [3, 1, 2, 1]);            // true
+areArraysEqual([1, 2, 3, 4], [1, 1, 2, 3]);            // false
+areArraysEqual([1, 1, 2, 2], [4, 2, 3, 1]);            // false
+areArraysEqual([1, 1, 2], [1, 2, 2]);                  // false
+areArraysEqual([1, 1, 1], [1, 1]);                     // false
+areArraysEqual([1, 1], [1, 1]);                        // true
