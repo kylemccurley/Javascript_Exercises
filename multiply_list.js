79fc486d7b804a8f0731a5e4eadbc7af481549ad
Kyle McCurley <kmccurley123@gmail.com>
2020-08-09 23:48:59 -0400

    Create multiply_list.js

---
+++ multiply_list.js
@@ -0,0 +1,10 @@
+function multiplyList(arr1, arr2) {
+  let outcome = [];
+  for (let i = 0; i < arr1.length; i++) {
+    outcome.push(arr1[i] * arr2[i]);
+  }
+
+  return outcome;
+}
+
+multiplyList([3, 5, 7], [9, 10, 11]);    // [27, 50, 77]
