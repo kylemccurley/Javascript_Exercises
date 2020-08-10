e6244138a826ae5880e80f0c46eb2b74ae8e2e89
Kyle McCurley <kmccurley123@gmail.com>
2020-08-09 23:43:10 -0400

    Create interleave.js

---
+++ interleave.js
@@ -0,0 +1,13 @@
+function interleave(arr1, arr2) {
+  let interleavedArrays = [];
+  for (let idx = 0; idx < arr1.length; idx++) {
+    let arr1El = arr1[idx];
+    let arr2El = arr2[idx];
+    interleavedArrays.push(arr1El);
+    interleavedArrays.push(arr2El);
+  }
+
+  return interleavedArrays;
+}
+
+interleave([1, 2, 3], ['a', 'b', 'c']);    // [1, "a", 2, "b", 3, "c"]
