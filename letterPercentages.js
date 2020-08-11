58646ede74fbebbc1385eee24a2be901d5955977
Kyle McCurley <kmccurley123@gmail.com>
2020-08-11 01:17:45 -0400

    Create letterPercentages.js

---
+++ letterPercentages.js
@@ -0,0 +1,21 @@
+ function letterPercentages(str) {
+  let outcome = { lowercase: 0, uppercase: 0, neither: 0 };
+  let uppercaseCount = str.match(/[A-Z]/g);
+  let lowercaseCount = str.match(/[a-z]/g);
+  let neitherCount = str.length - (uppercaseCount + lowercaseCount);
+
+  return {
+           uppercase: ((uppercaseCount / str.length) * 100)},
+           lowercase: ((lowercaseCount / str.length) * 100),
+           neither: ((neitherCount) / str.length) * 100), 
+          }
+}
+
+letterPercentages('abCdef 123');
+// { lowercase: "50.00", uppercase: "10.00", neither: "40.00" }
+
+letterPercentages('AbCd +Ef');
+// { lowercase: "37.50", uppercase: "37.50", neither: "25.00" }
+
+letterPercentages('123');
+// { lowercase: "0.00", uppercase: "0.00", neither: "100.00" }
