86299111bf738f05600e7b3eb2bea727dd0523e7
Kyle McCurley <kmccurley123@gmail.com>
2020-08-10 21:38:45 -0400

    Create rotate_array.js

---
+++ rotate_array.js
@@ -0,0 +1,31 @@
+function rotateArray(arr) {
+  if (!arr) {
+    return [];
+  }
+
+  let outcome = [];
+  for (let i = 1; i < arr.length; i++) {
+    let char = arr[i];
+    outcome.push(char);
+  }
+
+
+  return outcome.concat(arr[0]);
+}
+
+rotateArray([7, 3, 5, 2, 9, 1]);       // [3, 5, 2, 9, 1, 7]
+rotateArray(['a', 'b', 'c']);          // ["b", "c", "a"]
+rotateArray(['a']);                    // ["a"]
+rotateArray([1, 'a', 3, 'c']);         // ["a", 3, "c", 1]
+rotateArray([{ a: 2 }, [1, 2], 3]);    // [[1, 2], 3, { a: 2 }]
+rotateArray([]);                       // []
+
+// return `undefined` if the argument is not an array
+rotateArray();                         // undefined
+rotateArray(1);                        // undefined
+
+
+// the input array is not mutated
+const array = [1, 2, 3, 4];
+rotateArray(array);                    // [2, 3, 4, 1]
+array;                                 // [1, 2, 3, 4]
