ac6ec10fc9f878b333f5a178f27aa0016e529531
Kyle McCurley <kmccurley123@gmail.com>
2020-08-18 12:51:58 -0400

    Create filter.js

---
+++ filter.js
@@ -0,0 +1,20 @@
+function myFilter(array, func) {
+  let filteredElements = [];
+  for (let el of array) {
+    if (func(el)) {
+      filteredElements.push(el);
+    }
+  }
+
+  return filteredElements;
+}
+
+let isPythagoreanTriple = function (triple) {
+  return Math.pow(triple.a, 2) + Math.pow(triple.b, 2) === Math.pow(triple.c, 2);
+};
+
+myFilter([{ a: 3, b: 4,  c: 5 },
+          { a: 5, b: 12, c: 13 },
+          { a: 1, b: 2,  c: 3 },], isPythagoreanTriple);
+
+// returns [ { a: 3, b: 4, c: 5 }, { a: 5, b: 12, c: 13 } ]
