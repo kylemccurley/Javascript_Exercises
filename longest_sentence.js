5f6157b5e3591e7bfe3396506da7cfda1ebf7f6f
Kyle McCurley <kmccurley123@gmail.com>
2020-08-20 12:53:44 -0400

    Update longest_sentence.js

--- longest_sentence.js
+++ longest_sentence.js
@@ -51,8 +51,8 @@ longestSentence(longText);
 /*
 It is rather for us to be here dedicated to the great task remaining before us -- that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion -- that we here highly resolve that these dead shall not have died in vain -- that this nation, under God, shall have a new birth of freedom -- and that government of the people, by the people, for the people, shall not perish from the earth.
 */
-
 // The longest sentence has 86 words.
+
 longestSentence('...hello'); // Sentences starting with punctation -> Ignore punctuation.
 longestSentence('.?  how ? r u?'); // Sentences with words of 1 letter
 longestSentence("I''m not sure...what's the answer?'"); // Two of Same Length. Choose First. Quotes counted in word parsing
